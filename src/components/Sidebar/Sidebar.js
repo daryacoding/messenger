6edda9bb5fe7e9ebd0476aa960edfef51138917f
daryacoding <abdullahzadeh.darya@gmail.com>
2023-01-03 22:33:05 -0500

    search bar created

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import './Sidebar.css'
 import DonutLargeIcon from '@mui/icons-material/DonutLarge'
+import ChatIcon from '@mui/icons-material/Chat'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 import {Avatar, IconButton} from '@mui/material'
+import { SearchOutlined } from '@mui/icons-material'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar-header">
+                <Avatar src='https://w7.pngwing.com/pngs/4/736/png-transparent-female-avatar-girl-face-woman-user-flat-classy-users-icon.png'/>
                 <div className="sidebar-header-right">
                     <IconButton>
                         <DonutLargeIcon />
                     </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className='sidebar-search'>
+                <div className='sidebar-search-container'>
+                    <SearchOutlined />
+                    <input placeholder='Search or start new chat' type='text' />
                 </div>
             </div>
         </div>
