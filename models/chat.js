20391b8b442e405ee75f634adbf3c813f0c2359a
daryacoding <abdullahzadeh.darya@gmail.com>
2023-02-03 22:17:02 -0500

    chat model updated

--- models/chat.js
+++ models/chat.js
@@ -1,11 +1,18 @@
 const { model, Schema } = require('mongoose')
 
 const chatSchema = new Schema({
-    poster:{type: Schema.Types.ObjectId, ref: 'User'},
-    message: {required: true, type: String},
+    poster:{type: Schema.Types.ObjectId, 
+        ref: 'User'},
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    message: {required: true, 
+        type: String},
 },  {
     timestamps: true
 })
 
 const Chat = model('Chat', chatSchema)
+
 module.exports = Chat
